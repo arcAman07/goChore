aa4c4f64f8a1e6cadeab230664838a367b809466
Aman <aman.sharma2020b@vitstudent.ac.in>
2021-11-04 18:21:32 +0530

    config file added

--- go.mod
+++ go.mod
@@ -1,3 +1,12 @@
 module Go-Tasks
 
 go 1.17
+
+require (
+	github.com/cpuguy83/go-md2man/v2 v2.0.0-20190314233015-f79a8a8ca69d // indirect
+	github.com/lib/pq v1.10.3 // indirect
+	github.com/russross/blackfriday/v2 v2.0.1 // indirect
+	github.com/shurcooL/sanitized_anchor_name v1.0.0 // indirect
+	github.com/urfave/cli v1.22.5 // indirect
+	gopkg.in/mgo.v2 v2.0.0-20190816093944-a6b53ec6cb22 // indirect
+)
--- main.go
+++ main.go
@@ -2,8 +2,35 @@ package main
 
 import (
 	"fmt"
+	"os"
+
+	"github.com/urfave/cli"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	app := cli.NewApp()
+
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "lang",
+			Value: "english",
+			Usage: "language for the greeting",
+		},
+	}
+
+	app.Action = func(c *cli.Context) error {
+		name := "Nefertiti"
+		if c.NArg() > 0 {
+			name = c.Args().Get(0)
+		}
+		if c.String("lang") == "spanish" {
+			fmt.Println("Hola", name)
+		} else {
+			fmt.Println("Hello", name)
+		}
+		return nil
+	}
+
+	app.Run(os.Args)
+
 }
