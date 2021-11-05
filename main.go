c32afd5c73f5e64d432bf04de953daabed6bd1b7
Aman <aman.sharma2020b@vitstudent.ac.in>
2021-11-05 15:07:52 +0530

    logger added

--- main.go
+++ main.go
@@ -1,31 +1,7 @@
 package main
 
-import (
-	"os"
-
-	"github.com/urfave/cli"
-	"github.com/arcAman07/Go-Tasks/actions"
-)
-
 // Designing cli apps
-var app = 
 
 func main() {
-	app.Name = "Go-Tasks"
 
-	app.Commands = []cli.Command{
-		{
-			Name: "noop",
-		},
-		{
-			Name:     "add",
-			Category: "template",
-		},
-		{
-			Name:     "remove",
-			Category: "template",
-		},
-	}
-
-	app.Run(os.Args)
 }
