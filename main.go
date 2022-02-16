ed3cfb447fdb5223d452c98d4510855936f41478
Aman <aman.sharma2020b@vitstudent.ac.in>
2022-02-17 01:30:54 +0530

    api operations updated

--- main.go
+++ main.go
@@ -3,26 +3,25 @@ package main
 // Designing cli apps
 import (
 	"goChore/actions"
-	"fmt"
+	// "fmt"
 	"log"
 	"os"
-	"goChore/api/Users"
-	"goChore/api/Tasks"
-	"goChore/config"
+	// "goChore/api/Users"
+	// "goChore/api/Tasks"
+	// "goChore/config"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
 var app = actions.App
 var UserCollection *mongo.Collection
 var TaskCollection *mongo.Collection
-func init() {
-	UserCollection,TaskCollection = config.Configure()
-	Users.Insert(UserCollection)
-	Tasks.Insert(TaskCollection)
-}
+// func init() {
+// 	UserCollection,TaskCollection = config.Configure()
+// 	Users.Insert(UserCollection)
+// 	Tasks.Insert(TaskCollection)
+// }
 
 func main() {
-	fmt.Println("Hello World")
 	actions.Actions()
 	err := app.Run(os.Args)
 	if err != nil {
