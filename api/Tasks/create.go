a2855f3161081aa82f0e8d2a777dab4ac4802a5c
Aman <aman.sharma2020b@vitstudent.ac.in>
2022-02-16 22:20:23 +0530

    api operations updated

--- api/Tasks/create.go
+++ api/Tasks/create.go
@@ -1,1 +1,38 @@
 package Tasks
+import (
+	"fmt"
+	"context"
+	"goChore/models"
+	"go.mongodb.org/mongo-driver/mongo"
+	"time"
+)
+var currentTime time.Time = time.Now()
+var currentDay string = currentTime.Format("01-02-2006")
+func Insert(coll *mongo.Collection) {
+	// Insert a single document
+	p1 := models.Task{
+		TaskName:"study physics",
+		Date:currentDay,
+		Status:true,
+	}
+	p2 := models.Task{
+		TaskName:"code in golang",
+		Date:currentDay,
+		Status:false,
+	}
+	p3 := models.Task{
+		TaskName:"Eating Food",
+		Date:currentDay,
+		Status:true,
+	
+	}
+	// Insert multiple documents
+	multiTasks := []interface{}{p1, p2, p3}
+	result, err := coll.InsertMany(context.TODO(), multiTasks)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("Number of documents inserted: %d\n", len(result.InsertedIDs))
+
+}
