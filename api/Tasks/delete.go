cbd6d9cc67d50bef5e94cf8c4d60800405e88989
Aman <aman.sharma2020b@vitstudent.ac.in>
2022-02-16 22:35:20 +0530

    api operations updated

--- api/Tasks/delete.go
+++ api/Tasks/delete.go
@@ -1,1 +1,17 @@
 package Tasks
+import (
+	"fmt"
+	"context"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+func Delete(coll *mongo.Collection) {
+	// Delete a single document
+	filter := bson.M{"TaskName": "Eating Food"}
+	res, err := coll.DeleteOne(context.TODO(), filter)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("Number of documents deleted: %d\n", res.DeletedCount)
+}
