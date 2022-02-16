c658c264611911ed49dc29cfa7b7544f2c8b39be
Aman <aman.sharma2020b@vitstudent.ac.in>
2022-02-17 03:08:48 +0530

    task-cli updated

--- cli/Task-cli/addTask.go
+++ cli/Task-cli/addTask.go
@@ -4,12 +4,12 @@ import (
 	"os"
 )
 
-func AddTask() (string,string) {
+func AddTask() (string) {
 	command := os.Args[1]
 	username := os.Args[2]
 	if command == "login" {
 		fmt.Println("Logging in user")
 		return username
 	}
-	return "", ""
+	return ""
 }
