bd856c2ee4582f17d99a1ba73b85824b9aa935b3
Aman <aman.sharma2020b@vitstudent.ac.in>
2022-02-17 03:05:14 +0530

    api operations updated

--- cli/Task-cli/refreshTask.go
+++ cli/Task-cli/refreshTask.go
@@ -1,1 +1,16 @@
 package TaskCli
+import (
+	"fmt"
+	"os"
+)
+
+func LoginUser() (string, string) {
+	command := os.Args[1]
+	username := os.Args[2]
+	password := os.Args[3]
+	if command == "login" {
+		fmt.Println("Logging in user")
+		return username, password
+	}
+	return "", ""
+}
