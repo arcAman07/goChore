0dc829b4b7f8bf27631db859403d8a4d35189fde
Aman <aman.sharma2020b@vitstudent.ac.in>
2022-02-16 22:42:22 +0530

    api operations updated

--- api/Tasks/find.go
+++ api/Tasks/find.go
@@ -1,1 +1,35 @@
 package Tasks
+import (
+	"fmt"
+	"context"
+	"go.mongodb.org/mongo-driver/bson"
+	"goChore/models"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+func Find(coll *mongo.Collection) {
+	// Find all documents
+	cursor, err := coll.Find(context.TODO(), bson.M{})
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	// Iterate through the cursor
+	for cursor.Next(context.TODO()) {
+		var task models.Task
+		err := cursor.Decode(&task)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Printf("%+v\n", task)
+	}
+	if err := cursor.Err(); err != nil {
+		fmt.Println(err)
+		return
+	}
+	// Close the cursor once finished
+	if err := cursor.Close(context.TODO()); err != nil {
+		fmt.Println(err)
+		return
+	}
+}
