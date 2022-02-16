040f691c02eb897876fa101c8c799a9dd600d79a
Aman <aman.sharma2020b@vitstudent.ac.in>
2022-02-16 21:58:56 +0530

    Updated User model

--- api/Users/create.go
+++ api/Users/create.go
@@ -4,23 +4,24 @@ import (
 	"context"
 	"goChore/models"
 	"go.mongodb.org/mongo-driver/mongo"
+	"golang.org/x/crypto/bcrypt"
 )
 func Insert(coll *mongo.Collection) {
 	// Insert a single document
 	p1 := models.User{
-		Id:    1,
-		Name:  "John",
-		Email: "JOHN@GMAIL.com",
+		Username: "arcAman07",
+		Password: "12345",
+		HashedPassword:bcrypt.GenerateFromPassword([]byte("12345"), bcrypt.DefaultCost)
 	}
 	p2 := models.User{
-		Id:    2,
-		Name:  "Aman",
-		Email: "a@gmail.com",
+		Username: "john05",
+		Password: "123456",
+		HashedPassword: bcrypt.GenerateFromPassword([]byte("123456"), bcrypt.DefaultCost)
 	}
 	p3 := models.User{
-		Id:    3,
-		Name:  "Jane",
-		Email: "Jane@gmail.com",
+		Username: "janeb4",
+		Password: "1234567",
+		HashedPassword: bcrypt.GenerateFromPassword([]byte("1234567"), bcrypt.DefaultCost)
 	}
 	// Insert multiple documents
 	multiUsers := []interface{}{p1, p2, p3}
--- go.mod
+++ go.mod
@@ -18,8 +18,8 @@ require (
 	github.com/xdg-go/stringprep v1.0.2 // indirect
 	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
 	go.mongodb.org/mongo-driver v1.8.3 // indirect
-	golang.org/x/crypto v0.0.0-20201216223049-8b5274cf687f // indirect
+	golang.org/x/crypto v0.0.0-20220214200702-86341886e292 // indirect
 	golang.org/x/sync v0.0.0-20190911185100-cd5d95a43a6e // indirect
-	golang.org/x/text v0.3.5 // indirect
+	golang.org/x/text v0.3.6 // indirect
 	gopkg.in/mgo.v2 v2.0.0-20190816093944-a6b53ec6cb22 // indirect
 )
