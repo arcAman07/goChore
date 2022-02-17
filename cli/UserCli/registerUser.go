8eeb7058c407891032958d08e95a1e452fd2ec58
Aman <aman.sharma2020b@vitstudent.ac.in>
2022-02-17 12:28:13 +0530

    task-cli updated

--- cli/UserCli/registerUser.go
+++ cli/UserCli/registerUser.go
@@ -5,15 +5,17 @@ import (
 	"os"
 ) 
 // 0 means used is logged out
+// 1 means the user is logged in
 var loggedIn int = 0
 
-func RegisterUser() (string, string) {
+func RegisterUser() (string, string, int) {
 	command := os.Args[1]
 	username := os.Args[2]
 	password := os.Args[3]
 	if command == "register" {
+		loggedIn = 1
 		fmt.Println("Registering user")
-		return username, password
+		return username, password,loggedIn
 	}
-	return "", ""
+	return "", "",2
 }
