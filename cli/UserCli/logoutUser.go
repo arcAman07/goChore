55682f55ddd375438be030836ea11a0b002d8183
Aman <aman.sharma2020b@vitstudent.ac.in>
2022-02-17 15:20:40 +0530

    handlerUsers updated

--- cli/UserCli/logoutUser.go
+++ cli/UserCli/logoutUser.go
@@ -13,13 +13,13 @@ import (
 func LogoutUser(coll *mongo.Collection) {
 	command := os.Args[1]
 	username := os.Args[2]
+	if command == "logout" && LoggedIn == 0 {
+		fmt.Println("User is already logged out")
+		fmt.Println("Please login again")
+	}
 	if command == "logout" && LoggedIn == 1 {
 		LoggedIn = 0
 		fmt.Println("Logging out user ...")
 		handlerUsers.LogoutHandler(coll, username,LoggedIn)
-	}
-	if command == "logout" && LoggedIn == 0 {
-		fmt.Println("User is already logged out")
-		fmt.Println("Please login again")
 	}
 }
