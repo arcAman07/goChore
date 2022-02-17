8c228a7229067c92b73d589088ff7676f04f20d6
Aman <aman.sharma2020b@vitstudent.ac.in>
2022-02-17 21:33:41 +0530

    Task Handlers updated

--- cli/TaskCli/updateTask.go
+++ cli/TaskCli/updateTask.go
@@ -12,7 +12,7 @@ func UpdateTask(user *mongo.Collection,task *mongo.Collection) {
 	userName := os.Args[2]
 	currentTask := os.Args[3]
 	newTask := os.Args[4]
-	if command == "update"{
+	if command == "update" {
 		handlerTasks.UpdateTaskHandler(user,task,userName,currentTask,newTask)
 		return
 	}
