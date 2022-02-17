92bd46f75cb622d2c5d1fe19c45b504b9e5bfdb7
Aman <aman.sharma2020b@vitstudent.ac.in>
2022-02-17 12:06:20 +0530

    task-cli updated

--- cli/Task-cli/updateTask.go
+++ cli/Task-cli/updateTask.go
@@ -6,11 +6,11 @@ import (
 
 func UpdateTask() (string, string) {
 	command := os.Args[1]
-	username := os.Args[2]
-	password := os.Args[3]
-	if command == "login" {
-		fmt.Println("Logging in user")
-		return username, password
+	currentTask := os.Args[2]
+	newTask := os.Args[3]
+	if command == "update" {
+		fmt.Println("Updating the task...")
+		return currentTask, newTask
 	}
 	return "", ""
 }
