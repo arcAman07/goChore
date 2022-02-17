2cf9c82d06c301f0a2a27f0fcf03295d68438ecd
Aman <aman.sharma2020b@vitstudent.ac.in>
2022-02-17 13:39:25 +0530

    registerHandler added

--- api/Users/handlerUsers/registerHandler.go
+++ api/Users/handlerUsers/registerHandler.go
@@ -1,9 +1,35 @@
 package handlerUsers
 
 import (
+	"context"
+	"fmt"
+	"goChore/models"
 	"go.mongodb.org/mongo-driver/mongo"
+	"golang.org/x/crypto/bcrypt"
 )
 
-func RegisterHandler(coll *mongo.Collection,username string, password string, status int) int {
+func RegisterHandler(coll *mongo.Collection,username string, password string, status int)  {
+	Username := username
+	Password := password
+	LoggedIn := status
+	hashedPassword,err := bcrypt.GenerateFromPassword([]byte(Password), bcrypt.DefaultCost)
+	if err != nil {
+		fmt.Println(err)
+		fmt.Println("Error in hashing password")
+		return
+	}
+	user := models.User{
+		Username: Username,
+		Password: Password,
+		HashedPassword: (string)(hashedPassword),
+		LoggedIn:LoggedIn,
+	}
+	_ , err = coll.InsertOne(context.TODO(), user)
+	if err != nil {
+		fmt.Println(err)
+		fmt.Println("Registration failed")
+		return
+	}
+	fmt.Println("Registration successful")
 	
 }
