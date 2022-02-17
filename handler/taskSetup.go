9d624903776d0792529de0c5b9be0f1b591b50b5
Aman <aman.sharma2020b@vitstudent.ac.in>
2022-02-17 14:58:46 +0530

    handlerUsers updated

--- handler/taskSetup.go
+++ handler/taskSetup.go
@@ -8,4 +8,8 @@ func TaskSetup(task *mongo.Collection) {
 	TaskCli.AddTask(task)
 	TaskCli.DeleteTask(task)
 	TaskCli.FinishTask(task)
+	TaskCli.RemainTask(task)
+	TaskCli.RefreshTask(task)
+	TaskCli.UpdateTask(task)
+	TaskCli.ListTask(task)
 }
