31eac8e4b9c17eed04026425bbf8e59a7117bb81
Aman <aman.sharma2020b@vitstudent.ac.in>
2022-02-17 17:09:23 +0530

    handlerUsers updated

--- cli/TaskCli/addTask.go
+++ cli/TaskCli/addTask.go
@@ -11,8 +11,7 @@ import (
 func AddTask(task *mongo.Collection) (string) {
 	command := os.Args[1]
 	taskName := os.Args[2]
-	status := UserCli.LoggedIn
-	if command == "add" && status == 1 {
+	if command == "add" {
 		fmt.Println("Adding task")
 		return taskName
 	}
