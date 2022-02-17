5542483cbe7c052107d9e40d4cecade78651e7eb
Aman <aman.sharma2020b@vitstudent.ac.in>
2022-02-17 18:04:55 +0530

    Task Handlers updated

--- cli/TaskCli/refreshTask.go
+++ cli/TaskCli/refreshTask.go
@@ -1,9 +1,18 @@
 package TaskCli
 
 import (
-	"fmt"
-	"goChore/cli/UserCli"
 	"os"
 	"go.mongodb.org/mongo-driver/mongo"
+	"goChore/api/Tasks/handlerTasks"
 )
 
+func RefreshTask(user *mongo.Collection,task *mongo.Collection) {
+	command := os.Args[1]
+	userName := os.Args[2]
+	taskName := os.Args[3]
+	if command == "refresh" {
+		handlerTasks.RefreshTaskHandler(user,task,userName, taskName)
+		return
+	}
+	return 
+}
