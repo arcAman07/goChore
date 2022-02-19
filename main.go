94f642230dee0c03d9845547a84624a69da1905d
Aman <aman.sharma2020b@vitstudent.ac.in>
2022-02-19 18:53:36 +0530

    Formatted code updated

--- main.go
+++ main.go
@@ -11,10 +11,13 @@ import (
 var UserCollection *mongo.Collection
 var TaskCollection *mongo.Collection
 
-func main() {
+func init() {
 	UserCollection, TaskCollection = config.Configure()
 	handler.UserSetup(UserCollection)
 	handler.TaskSetup(UserCollection, TaskCollection)
+}
+
+func main() {
 }
 
 // func init will be used to populate some raw dummy data in the database
