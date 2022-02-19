42b1fd37b2d957c644382443c3f82a8679b6e3dd
Aman <aman.sharma2020b@vitstudent.ac.in>
2022-02-20 03:28:27 +0530

    Formatted code updated

---
+++ api/Tasks/handlerTasks/statsTaskHandler.go
@@ -0,0 +1,84 @@
+package handlerTasks
+
+import (
+	"context"
+	"fmt"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"goChore/models"
+)
+
+func StatsTaskHandler(user *mongo.Collection, task *mongo.Collection, username string) {
+	var Username string = username
+	Userfilter := bson.M{"Username": Username}
+	cursor, err := user.Find(context.TODO(), Userfilter)
+	var doneTasks int = 0
+	var pendingTasks int = 0
+	Taskfilter := bson.M{}
+	if err != nil {
+		fmt.Println("Username does not exist")
+		fmt.Println("Please register")
+		return
+	}
+	// Iterate through the cursor
+	for cursor.Next(context.TODO()) {
+		var user models.User
+		err := cursor.Decode(&user)
+		if err != nil {
+			fmt.Println(err)
+			fmt.Println("Error in decoding user")
+			return
+		}
+		if user.LoggedIn == 1 {
+			fmt.Println("Updating task status ...")
+			cursor, err = task.Find(context.TODO(), Taskfilter)
+			if err != nil {
+				fmt.Println(err)
+				fmt.Println("Error in updating task status")
+				fmt.Println("Please check task name")
+				return
+			}
+			for cursor.Next(context.TODO()) {
+				var task models.Task
+				err := cursor.Decode(&task)
+				if err != nil {
+					fmt.Println(err)
+					fmt.Println("Error in decoding task")
+					return
+				}
+				if task.Status == "Done" {
+					doneTasks++
+				}
+				if task.Status == "Not Done" {
+					pendingTasks++
+				}
+
+			}
+			if err := cursor.Err(); err != nil {
+				fmt.Println(err)
+				return
+			}
+			// Close the cursor once finished
+			if err := cursor.Close(context.TODO()); err != nil {
+				fmt.Println(err)
+				return
+			}
+		}
+		if user.LoggedIn == 0 {
+			fmt.Println("Please login first")
+			return
+		}
+		if err := cursor.Err(); err != nil {
+			fmt.Println(err)
+			fmt.Println("Error in cursor")
+			return
+		}
+		// Close the cursor once finished
+		if err := cursor.Close(context.TODO()); err != nil {
+			fmt.Println(err)
+			fmt.Println("Error in closing cursor")
+			return
+		}
+
+	}
+}
---
+++ cli/TaskCli/statsTask.go
@@ -0,0 +1,16 @@
+package TaskCli
+
+import (
+	"go.mongodb.org/mongo-driver/mongo"
+	"goChore/api/Tasks/handlerTasks"
+	"os"
+)
+
+func StatsTask(user *mongo.Collection, task *mongo.Collection) {
+	command := os.Args[1]
+	userName := os.Args[2]
+	if command == "stats" {
+		handlerTasks.AddTaskHandler(user, task, userName)
+		return
+	}
+}
