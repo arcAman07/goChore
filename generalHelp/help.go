1a8709a4da42f3ac5e3f541b0141d999405666e2
Aman <aman.sharma2020b@vitstudent.ac.in>
2022-02-20 02:41:06 +0530

    Formatted code updated

--- generalHelp/help.go
+++ generalHelp/help.go
@@ -1,8 +1,15 @@
 package generalHelp
 import (
 	"fmt"
+	"os"
+	"github.com/fatih/color"
 )
 
+var i int = 1
 func GeneralHelp() {
-	fmt.Println("Hello, playground")
+	color.Red("I am a god")
+	command := os.Args[1]
+	if command == "help" {
+		fmt.Printf("%d\t%+v\n",i,"help")
+	}
 }
