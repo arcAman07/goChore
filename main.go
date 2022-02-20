e3561badbb6fc48d2d7d5c505e2277479bf9594f
Aman <aman.sharma2020b@vitstudent.ac.in>
2022-02-20 18:27:40 +0530

    Formatted code updated

--- main.go
+++ main.go
@@ -12,28 +12,18 @@ var UserCollection *mongo.Collection
 var TaskCollection *mongo.Collection
 
 func init() {
-	UserCollection, TaskCollection = config.Configure()
-	handler.UserSetup(UserCollection)
-	handler.TaskSetup(UserCollection, TaskCollection)
-	handler.Helper()
+
 }
 
 func main() {
 
+	UserCollection, TaskCollection = config.Configure()
+	handler.UserSetup(UserCollection)
+	handler.TaskSetup(UserCollection, TaskCollection)
+	handler.Helper()
+
 }
 
-// func init will be used to populate some raw dummy data in the database
-
-// Have to be implemented
-
-// Write test code for testing the functions, and implement logger
 
 // Better auth techniques to be added ( tokens ), CI-CD pipelines
 
-// better errer handling and use of go routines
-
-// Addition of assets ( beautiful CLI designs )
-
-// Sort of help command which will display all commands and their information
-
-// Adding environment variables with mongodb cluster and colors library in golang
