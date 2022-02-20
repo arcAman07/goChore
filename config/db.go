91b51432c216e9c7896f12ff53623eee20be31c7
Aman <aman.sharma2020b@vitstudent.ac.in>
2022-02-20 18:25:20 +0530

    Formatted code updated

--- config/db.go
+++ config/db.go
@@ -3,19 +3,44 @@ package config
 import (
 	"context"
 	"fmt"
+	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
+	"log"
+	"os"
+	"time"
 )
 
-const uri = "mongodb://localhost:27017/taskDB"
+// const uri = "mongodb://localhost:27017/taskDB"
 
 func Configure() (*mongo.Collection, *mongo.Collection) {
 	// Create a new client and connect to the server
-	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
+
+	err := godotenv.Load()
 	if err != nil {
-		panic(err)
+		log.Fatal("Error loading .env file")
 	}
+	uri := os.Getenv("MONGO_URI")
+	fmt.Println(uri)
+
+	serverAPIOptions := options.ServerAPI(options.ServerAPIVersion1)
+
+	clientOptions := options.Client().
+		ApplyURI(uri).
+		SetServerAPIOptions(serverAPIOptions)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	client, err := mongo.Connect(ctx, clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
+	// if err != nil {
+	// 	panic(err)
+	// }
 
 	// Close the connection when the function ends
 
